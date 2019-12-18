1d3097fcd0e5eb98f9082faad25e1417f043a9f3
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-12-18 12:56:09 -0500

    Add project files.

---
+++ Boxes/Box.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boxes
+{
+    class Box
+    {
+
+        //public double height { get; set; }
+        double height;
+        double length;
+        double width;
+        double density;
+
+        public string Label;
+
+
+        public double madeUp { get; } //testing read-only syntax
+
+        double surfaceArea;
+        double volume;
+        double mass;
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+                volume = findVolume(height, length, width);
+                surfaceArea = findSurfaceArea(height, length, width);
+                mass = findMass(density, volume);
+            }
+        }
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value;
+                volume = findVolume(height, length, width);
+                surfaceArea = findSurfaceArea(height, length, width);
+                mass = findMass(density, volume);
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+                volume = findVolume(height, length, width);
+                surfaceArea = findSurfaceArea(height, length, width);
+                mass = findMass(density, volume);
+            }
+        }
+
+        public double Density
+        {
+            get
+            {
+                return density;
+            }
+            set
+            {
+                density = value;
+                mass = findMass(density, volume);
+            }
+        }
+
+        /// <summary>
+        /// The volume of this box
+        /// </summary>
+        public double Volume
+        {
+            get
+            {
+                return volume;
+            }
+        }
+
+        /// <summary>
+        /// The surface area of this box
+        /// </summary>
+        public double SurfaceArea
+        {
+            get
+            {
+                return surfaceArea;
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return mass;
+            }
+        }
+        /// <summary>
+        /// Constructor that sets default values
+        /// </summary>
+        public Box()
+        {
+            Height = 1;
+            Length = 1;
+            Width = 1;
+            Density = 1;
+
+
+            madeUp = 10;
+        }
+        /// <summary>
+        /// Constructor that sets height, length, and width at construction
+        /// </summary>
+        public Box(double H, double L, double W)
+        {
+            Height = H;
+            Length = L;
+            Width = W;
+            Density = 1;
+
+            madeUp = 10;
+        }
+
+        /// <summary>
+        /// Constructor that sets height, length, width, and density at construction
+        /// </summary>
+        /// <param name="H"></param>
+        /// <param name="L"></param>
+        /// <param name="W"></param>
+        /// <param name="D"></param>
+        public Box(double H, double L, double W, double D)
+        {
+            Height = H;
+            Length = L;
+            Width = W;
+            Density = D;
+
+            madeUp = 10;
+        }
+
+       
+
+
+        public static double findVolume(double H, double L, double W)
+        {
+            double V; //volume
+
+            V = H * L * W;
+            return V;
+        }
+
+        public static double findSurfaceArea(double H, double L, double W)
+        {
+            double sArea;
+            sArea = 2 * ( H * L +  L * W +  H * W);
+            return sArea;
+        }
+
+        public static double findMass(double d, double v)
+        {
+            double m;
+            m = d * v;
+            return m;
+        }
+
+        public string ShippingID()
+        {
+            string id = String.Format(Label.GetHashCode("04468")+ "v" + volume + "m" + Mass);
+
+            return id;
+        }
+    }
+}
---
+++ Boxes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using static Boxes.Box;
+
+namespace Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box();
+            
+            WriteLine("Volume is {0}, Surface Area is {1}", box1.Volume, box1.SurfaceArea);
+
+            Box box2 = new Box(2, 3, 3);
+
+            WriteLine("Volume is {0}, Surface Area is {1}", box2.Volume, box2.SurfaceArea);
+
+            box2.Length = 7;
+            box2.Label = "OXxyBox";
+            WriteLine("Volume is {0}, Surface Area is {1}", box2.Volume, box2.SurfaceArea);
+
+            WriteLine("Volume is {0}, Surface Area is {1}", Box.findVolume(2,3,3), Box.findSurfaceArea(2,3,3));
+
+            WriteLine(box2.ShippingID());
+        }
+    }
+}
